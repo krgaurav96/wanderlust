8a650be1e1ee89eadadfdce6361ae977709e41d0
Gaurav Kumar <gauravkumarbth@gmail.com>
2025-08-09 00:47:27 +0530

    Add Project Files

---
+++ init/index.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const initdata = require("./data.js");
+const Listing = require("../models/listing.js");
+
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+.then(()=>{
+    console.log("Connected To DB");
+})
+.catch((err)=>{
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+    
+}
+
+
+const initDB = async () => {
+    await Listing.deleteMany({});
+     initdata.data = initdata.data.map((obj) =>({...obj, owner: "6891f62198c8b6bab39ce81c"}));
+    await Listing.insertMany(initdata.data);
+    console.log("Data Was Initialized");
+
+};
+initDB();
+
