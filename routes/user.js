8f3462a0eb81de9d8665f3c91f69222f0691550d
Gaurav Kumar <gauravkumarbth@gmail.com>
2025-08-09 23:13:56 +0530

    Fix: ensure all userController functions are defined

--- routes/user.js
+++ routes/user.js
@@ -27,32 +27,44 @@
 
 // module.exports = router;
 
-const express = require("express");
-const wrapAsync = require("../utils/wrapAsync");
-const router = express.Router();
-const User = require("../models/user.js");
-const passport = require("passport");
-const { saveRedirectUrl } = require("../middleware.js");
-const userController = require("../controllers/users.js");
+const User = require("../models/user");
 
-router
-  .route("/signup")
-  .get(userController.renderSignupForm)
-  .post(wrapAsync(userController.signup));
+module.exports.renderSignupForm = (req, res) => {
+  res.render("users/signup");
+};
 
-router
-  .route("/login")
-  .get(userController.renderLoginForm) // ✅ fixed typo
-  .post(
-    saveRedirectUrl,
-    passport.authenticate("local", {
-      failureRedirect: "/login",
-      failureFlash: true,
-    }),
-    userController.login
-  );
+module.exports.signup = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+    const user = new User({ username, email });
+    const registeredUser = await User.register(user, password);
+    req.login(registeredUser, (err) => {
+      if (err) return next(err);
+      req.flash("success", "Welcome to Wanderlust!");
+      res.redirect("/listings");
+    });
+  } catch (e) {
+    req.flash("error", e.message);
+    res.redirect("/signup");
+  }
+};
 
-router.get("/logout", userController.logout);
+module.exports.renderLoginForm = (req, res) => {
+  res.render("users/login");
+};
 
-module.exports = router;
+module.exports.login = (req, res) => {
+  req.flash("success", "Welcome back!");
+  const redirectUrl = res.locals.redirectUrl || "/listings";
+  res.redirect(redirectUrl);
+};
+
+module.exports.logout = (req, res, next) => {
+  req.logout((err) => {
+    if (err) return next(err);
+    req.flash("success", "Goodbye!");
+    res.redirect("/listings");
+  });
+};
+
 
